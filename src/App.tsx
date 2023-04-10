613103c4e1aa1a4e3f1c4893184e9ba23e033fe0
risangdevs <88659300+risangdevs@users.noreply.github.com>
2023-04-10 11:14:52 +0700

    layout finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import { useState } from "react";
+import { Layout } from "./layout/Layout";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div></div>
-      <h1 className="">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Layout>
+        <p>lalala</p>
+      </Layout>
     </div>
   );
 }
